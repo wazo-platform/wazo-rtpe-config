6debb1c84e44c73f95528eff5c87ae4fb7c588c7
Aleksandar Sosic <alex.sosic@gmail.com>
2019-11-17 22:16:01 +0100

    Enh: move config to bootstrap

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,23 @@
 date
 
 PUBLIC_IP=$(ip -o -4 a | awk '$2 == "eth0" { gsub(/\/.*/, "", $4); print $4 }')
-sed -i -e "s/interface=PUBLIC_IP/interface=$PUBLIC_IP/g" /etc/rtpengine/rtpengine.conf
-sed -i -e "s/listen-ng=LISTEN_NG/listen-ng=$LISTEN_NG/g" /etc/rtpengine/rtpengine.conf
-sed -i -e "s/redis=REDIS/redis=$REDIS/g" /etc/rtpengine/rtpengine.conf
-sed -i -e "s/subscribe-keyspace=SUBSCRIBE_KEYSPACE/subscribe-keyspace=$SUBSCRIBE_KEYSPACE/g" /etc/rtpengine/rtpengine.conf
+
+cat << EOF > /etc/rtpengine/rtpengine.conf
+[rtpengine]
+interface=$PUBLIC_IP
+foreground=true
+log-stderr=true
+listen-ng=$LISTEN_NG
+port-min=23000
+port-max=32768
+recording-dir=/tmp
+recording-method=pcap
+recording-format=eth
+log-level=6
+delete-delay=0
+redis=$REDIS
+# subscribe-keyspace=$SUBSCRIBE_KEYSPACE
+EOF
+
 
 rtpengine --config-file /etc/rtpengine/rtpengine.conf
