19c02029e549daf8e32ad7fc4bb39003c4a61e79
Aleksandar Sosic <alex.sosic@gmail.com>
2019-11-21 23:20:05 +0100

    Enh: add consul support

--- Makefile
+++ Makefile
@@ -3,4 +3,4 @@ dockerfile: docker-wazo-rtpe
 
 .PHONY: docker-wazo-rtpe
 docker-wazo-rtpe:
-	docker build -t wazo-rtpe:latest -f Dockerfile .
+	docker build -t wazopbx/wazo-rtpe:latest -f Dockerfile .
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 date
 
+HOSTNAME=$(hostname)
+IP_ADDRESS=$(hostname -i)
 PUBLIC_IP=$(ip -o -4 a | awk '$2 == "eth0" { gsub(/\/.*/, "", $4); print $4 }')
 
 cat << EOF > /etc/rtpengine/rtpengine.conf
@@ -19,5 +21,10 @@ delete-delay=0
 redis=$REDIS
 EOF
 
+if ! [ -z "$WITH_CONSUL" ]; then
+    curl -X PUT \
+    -d '{"ID": "'$HOSTNAME'", "Name": "rtp", "Tags": [ "rtp", "rtpengine" ], "Address": "'$IP_ADDRESS'", "Port": '$LISTEN_NG'}' \
+    $CONSUL_URI:$CONSUL_PORT/v1/agent/service/register
+fi
 
 rtpengine --config-file /etc/rtpengine/rtpengine.conf
